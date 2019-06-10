49d14555efd4b60526eef44cf7552ff2fd764757
ram <ram@gmail.com>
2019-06-10 09:52:14 +0530

    first commit

---
+++ src/main/java/com/cloudies/demo/App.java
@@ -0,0 +1,13 @@
+package com.cloudies.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
