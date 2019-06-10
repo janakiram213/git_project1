5edce33ace589ef37f178fc4e85611c35135aa40
janakiram213 <janakiram213@gmail.com>
2019-06-10 19:08:19 +0530

    Update App.java

--- src/main/java/com/cloudies/demo/App.java
+++ src/main/java/com/cloudies/demo/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Welcome to Cloudseals pvt ltd" );
+        System.out.println("Aim bigger, Achieve smarter");
     }
 }
