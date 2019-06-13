7d1e3dbc07b8271ac42025e269a542360fc1ea78
EC2 Default User <ec2-user@ip-172-31-86-10.ec2.internal>
2019-06-13 06:12:16 +0000

    3rd commit

--- src/main/java/com/cloudies/demo/App.java
+++ src/main/java/com/cloudies/demo/App.java
@@ -8,10 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Welcome to Cloudseals pvt ltd" );
+        System.out.println( "Hello Welcome to CLOUDSEALS technologies,Ltd" );
         System.out.println("Aim bigger,Achieve smarter");
 	System.out.println("suchitra, hyderabad");
-	System.out.println("be happy");
+
     }
 
 }
